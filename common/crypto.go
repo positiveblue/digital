b000a1b7174f4999768dd7e6195e17640b286c13
jomsdev <jomsdev@gmail>
2017-12-25 18:49:16 +0100

    PKCS7 Padding func added

--- common/crypto.go
+++ common/crypto.go
@@ -1,7 +1,9 @@
 package common
 
 import (
+	"bytes"
 	"crypto/sha256"
+	"fmt"
 )
 
 // Sha256 computes the SHA256 of an array of bytes
@@ -10,4 +12,42 @@ func Sha256(data []byte) []byte {
 	hash := sha256.New()
 	hash.Write(data)
 	return hash.Sum(nil)
+}
+
+// pkcs7Pad appends padding following the PKCS7
+// more information here https://en.wikipedia.org/wiki/Padding_%28cryptography%29#PKCS7
+func pkcs7Pad(data []byte, blocklen int) ([]byte, error) {
+	if blocklen <= 0 {
+		return nil, fmt.Errorf("invalid blocklen %d", blocklen)
+	}
+	padlen := 1
+	for ((len(data) + padlen) % blocklen) != 0 {
+		padlen = padlen + 1
+	}
+
+	pad := bytes.Repeat([]byte{byte(padlen)}, padlen)
+	return append(data, pad...), nil
+}
+
+// pkcs7Unpad returns slice of the original data without padding
+func pkcs7Unpad(data []byte, blocklen int) ([]byte, error) {
+	if blocklen <= 0 {
+		return nil, fmt.Errorf("invalid blocklen %d", blocklen)
+	}
+	if len(data)%blocklen != 0 || len(data) == 0 {
+		return nil, fmt.Errorf("invalid data len %d", len(data))
+	}
+	padlen := int(data[len(data)-1])
+	if padlen > blocklen || padlen == 0 {
+		return nil, fmt.Errorf("invalid padding")
+	}
+	// check padding
+	pad := data[len(data)-padlen:]
+	for i := 0; i < padlen; i++ {
+		if pad[i] != byte(padlen) {
+			return nil, fmt.Errorf("invalid padding")
+		}
+	}
+
+	return data[:len(data)-padlen], nil
 }
