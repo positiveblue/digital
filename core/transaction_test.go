0f139559384db08cd073577fca44225fdee4fc73
Jordi Montes <jordimontes@acm.org>
2017-12-27 19:37:46 +0100

    MockTransacctions added to tests

--- core/transaction_test.go
+++ core/transaction_test.go
@@ -6,14 +6,37 @@ import (
 	"github.com/jomsdev/digital/common"
 )
 
+func MockTransactions() []Transaction {
+	var transactions []Transaction
+
+	keys := MockKeyPairs()
+
+	tr0 := NewTransaction(keys[0].Public, keys[0].Public, CoinCreation, []byte("First CoinCreation"))
+	tr0.SignTransaction(&keys[0])
+
+	tr1 := NewTransaction(keys[0].Public, keys[0].Public, CoinCreation, []byte("Second CoinCreation"))
+	tr1.SignTransaction(&keys[0])
+
+	tr2 := NewTransaction(keys[0].Public, keys[1].Public, Payment, []byte("Giving one to the first user"))
+	tr2.SignTransaction(&keys[0])
+
+	tr3 := NewTransaction(keys[0].Public, keys[2].Public, Payment, []byte("Giving one to the second user"))
+	tr3.SignTransaction(&keys[0])
+
+	tr4 := NewTransaction(keys[1].Public, keys[2].Public, Payment, []byte("Giving one to the second user"))
+	tr4.SignTransaction(&keys[1])
+	transactions = append(transactions, *tr0, *tr1, *tr2, *tr3, *tr4)
+
+	return transactions
+}
 func TestTransactionVerification(t *testing.T) {
 
-	kp, _ := GenerateNewKeypair()
+	kp := MockKeyPairs()[0]
 	randomPayload := []byte(common.RandStringBytesMaskImprSrc(1024))
 
 	tr := NewTransaction(kp.Public, nil, Payment, randomPayload)
 
-	tr.SignTransaction(kp)
+	tr.SignTransaction(&kp)
 
 	res, err := tr.VerifyTransaction()
 
@@ -24,13 +47,13 @@ func TestTransactionVerification(t *testing.T) {
 
 func TestTransactionVerificationFalseSignature(t *testing.T) {
 
-	kp, _ := GenerateNewKeypair()
-	kp2, _ := GenerateNewKeypair()
+	kp := MockKeyPairs()[0]
+	kp2 := MockKeyPairs()[1]
 	randomPayload := []byte(common.RandStringBytesMaskImprSrc(1024))
 
 	tr := NewTransaction(kp.Public, nil, Payment, randomPayload)
 
-	tr.SignTransaction(kp2)
+	tr.SignTransaction(&kp2)
 
 	res, err := tr.VerifyTransaction()
 
